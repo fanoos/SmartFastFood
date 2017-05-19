f772b8f3b5d5e7f84af33429d8dc8604b0878682
nik94 <fede.nik94@gmail.com>
2017-05-19 16:02:13 +0200

    fix employ 3

--- app/src/main/java/com/example/fede_xps/smartfastfood/HomeEmployActivity.java
+++ app/src/main/java/com/example/fede_xps/smartfastfood/HomeEmployActivity.java
@@ -20,7 +20,7 @@ public class HomeEmployActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vendor);
+        setContentView(R.layout.activity_home_employ);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
